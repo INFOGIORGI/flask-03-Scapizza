978d8917bb0aa4bd1582b8a26d49a63920ce95d5
edoardo <105643778+Scapizza@users.noreply.github.com>
2024-12-13 12:37:13 +0000

    Changes to be committed: 	modified:   app.py 	new file:   templates/base.html     new file:   templates/details.html 	new file:   templates/dettagliProdotto.html 	new file:   templates/index.html
    dddd
    
    ff

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("index.html", titolo="Homepage")
 
-app.run()
+@app.route("/details")
+def details():
+    prodotti = (("pennette", "pasta", 2),("prosciutto", "affettati", 1.5), ("armadio", "mobili", 120), ("Monopoli", "giochi", 8))
+    return render_template("details.html", titolo="Details", prodotti=prodotti)
+
+@app.route("/dettagliProdotto/<prodotto>")
+def dettagliProdotto(prodotto):
+
+
+
+
+app.run(debug=True)
